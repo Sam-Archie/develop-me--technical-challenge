34f5bc1357a2c4b33bf5da789f977ce72628ea76
Sam Archie <samuelarchie1@gmail.com>
2021-03-30 13:55:10 +0100

    created functionality for assigning players to games

--- src/data/reducers.js
+++ src/data/reducers.js
@@ -3,9 +3,24 @@ const startTournament = (state, {payload}) => ({...state, listOfPlayers: [...pay
 
 const randomizer = (state) => ({...state, listOfPlayers: state.listOfPlayers.sort(() => Math.random() - 0.5)});
 
+const createGamesFromArray = listOfPlayers => {
+    return listOfPlayers.reduce((acc, player, index) => {
+    if (index % 2 === 1) {
+        return acc;
+    }
+    const game = {
+        playerOne: player,
+        playerTwo: listOfPlayers[index + 1],
+    }
+    return [...acc, game];
+
+},[])}
+
+const createGames = (state) => ({...state, listOfRounds: createGamesFromArray(state.listOfPlayers)})
+
 const reducer = (state, action) => {
     switch (action.type) {
-        case "START_TOURNAMENT" : return randomizer(startTournament(state, action));
+        case "START_TOURNAMENT" : return createGames(randomizer(startTournament(state, action)));
         default : return state;
     }
 }
